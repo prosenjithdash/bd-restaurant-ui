298bcd2f925daff5a0803b554a061fa26f069437
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2024-05-31 07:09:45 +0600

    Updated navbar

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -2,7 +2,7 @@
 const Error = () => {
     return (
         <div>
-            <h2 className="text-[400px] font-extrabold text-center mt-[100px] text-orange-600">404</h2>
+            <h2 className="lg:text-[400px] text-[150px] font-extrabold text-center mt-[100px] text-orange-600">404</h2>
             <p className="text-center font-extrabold">Not Found</p>
         </div>
     );
